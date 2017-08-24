72b578201df13578c931b5c0068d4f4f337aeea0
Carsten Hufe <chufe@maprtech.com>
2017-08-25 00:40:00 +0200

    more notes added

--- clustercheck-core/src/main/groovy/com/mapr/emea/ps/clustercheck/core/ClusterCheckResult.groovy
+++ clustercheck-core/src/main/groovy/com/mapr/emea/ps/clustercheck/core/ClusterCheckResult.groovy
@@ -3,7 +3,7 @@ package com.mapr.emea.ps.clustercheck.core
 /**
  * Created by chufe on 22.08.17.
  */
-// TODO add files list and persist files in module folder
+// TODO add files list and persist files in module folder (e.g. for yarn-site.xml)
 class ClusterCheckResult {
     Object reportJson; // set Map or List
     String reportText;
